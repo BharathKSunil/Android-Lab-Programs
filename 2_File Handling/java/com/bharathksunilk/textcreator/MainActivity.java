08b79549a2802f829a27b7c1f8160d5deebe2d1f
BharathKSunil <bharathk.sunil.k@gmail.com>
2017-12-06 16:59:09 +0530

    Made class easier to switch from internal to external storage
    Signed-off-by: BharathKSunil <bharathk.sunil.k@gmail.com>

--- 2_File Handling/java/com/bharathksunilk/textcreator/MainActivity.java
+++ 2_File Handling/java/com/bharathksunilk/textcreator/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
@@ -20,7 +21,6 @@ import java.io.InputStreamReader;
 
 /**
  * In this class we are trying to perform read and write operations on android
- * We are currently using the internal storage to do the read an write operations
  */
 public class MainActivity extends Activity implements View.OnClickListener{
 
@@ -34,6 +34,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         tv_file=(TextView)findViewById(R.id.tv_file);
         et_content=(EditText)findViewById(R.id.et_content);
+
         setViewClickListeners(R.id.btn_create, R.id.btn_open, R.id.btn_save);
     }
 
@@ -54,8 +55,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 createFileCreateDialogue();
                 break;
             case R.id.btn_open:
-                createFileOpenDialogue();//use this while on emulator
-                //openFile();//use this while on device
+                //createFileOpenDialogue();//todo: use this while using internal storage
+                openFile();//use this while using external storage
                 break;
             case R.id.btn_save:
                 writeContentToFile(et_content.getText().toString());
@@ -63,37 +64,43 @@ public class MainActivity extends Activity implements View.OnClickListener{
         }
 
     }
-    /*
-    This Method is required if you are testing on a real phone
+
+    //todo remove this method if you are using internal storage
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode==RESULT_OK)
         {
             filePath=data.getData().getPath();
-            readContentsFromFile(filePath);
+            readContentsFromFile();
             tv_file.setText(filePath);
         }
         else
         {
             Toast.makeText(this,"Wrong Choice of File", Toast.LENGTH_LONG).show();
         }
-    }*/
+    }
 
     void createFile(String filename){
-        //filePath= Environment.getExternalStorageDirectory().getPath()+OUR_DIRECTORY; //external storage
-        filePath = getFilesDir().getPath()+"/";//internal storage
+        //external storage to documents folder
+        filePath= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath()+"/";
+        //filePath = getFilesDir().getPath()+"/";//todo: use this when using internal storage
         Log.i("BKS", filePath);
         File f=new File(filePath +=filename);
         try {
-            if (!f.createNewFile()){
+            if (f.createNewFile()){
                 Toast.makeText(MainActivity.this,
-                        "New File created in internal storage",
+                        "New File created in External storage",
                         Toast.LENGTH_SHORT).show();
                 tv_file.setText(filePath);
+            }else {
+                Toast.makeText(MainActivity.this,
+                        "The File Already Exists",
+                        Toast.LENGTH_SHORT).show();
             }
         } catch (IOException e) {
             e.printStackTrace();
+            Log.d("BKS", filePath);
             Toast.makeText(getBaseContext(), ""+e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
         }
     }
@@ -123,6 +130,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     /**
+	 * todo remove this method if you are using internal storage
      * This function creates an intent to connect to the device's file viewer to choose a file
      */
     void openFile(){
@@ -132,7 +140,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     /**
-     * This function mocks that it is opening the file
+     * This function opens the file from the internal storage
      * @param filename the name of the file to be opened
      */
     void openFile(String filename){
@@ -142,7 +150,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     /**
      * This function creates a dialog to enter the name of the file the user is trying to open
-     * This method is only required when you are working with emulators where external storage allocation must exist
+     * This method is only required when you are working with internal storage
+     * as the FileViewer doesn't have permission to view files in your apps internal storage directory
      */
     void createFileOpenDialogue(){
         final View dialogView=getLayoutInflater().inflate(R.layout.dialog_filename, null);
@@ -174,6 +183,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
             InputStreamReader inputReader= new InputStreamReader(fileIn);
             BufferedReader br=new BufferedReader(inputReader);
             String data=br.readLine();
+            et_content.setText("");
             while(data!=null)
             {
                 et_content.append(data+"\n");
